411a98dd9354866a5f369885834aeb9fee8cca1a
jsanchez92 <jiubersanchez@gmail.com>
2025-08-29 19:44:41 -0600

    AdminLTE

---
+++ Sistema_Matricula/resources/views/test.blade.php
@@ -0,0 +1,23 @@
+@extends('adminlte::page')
+
+@section('title', 'Test')
+@section('css')
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.24/css/jquery.dataTables.css">
+    <!-- Estilos de DataTables Buttons -->
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/buttons/1.7.1/css/buttons.dataTables.min.css">
+@stop
+
+@section('content_header')
+       <h1>Hola Mundo.</h1>
+@stop
+
+@section('content')
+@stop
+
+
+
+@section('js')
+<script> console.log('Hi!'); </script>
+   
+@stop
